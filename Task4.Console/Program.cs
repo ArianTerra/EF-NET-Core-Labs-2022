31fbeb4a3e8efbc4a3afd7f6eca659fa64214e14
ArianTerra <arianterra@gmail.com>
2022-12-30 19:47:17 +0200

    Task4 part 2 finished

--- Task4.Console/Program.cs
+++ Task4.Console/Program.cs
@@ -1,3 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
 
-Console.WriteLine("Hello, World!");
+using var conn = new SqlConnection("Server=(localdb)\\MSSQLLocalDB;DataBase=Task4.SQL;");
+conn.Open();
+
+// PrimeNumbers call example
+Console.WriteLine("PRIMES: ");
+var cmd  = new SqlCommand("PrimeNumbers", conn);
+cmd.CommandType = CommandType.StoredProcedure;
+cmd.Parameters.Add(new SqlParameter("@PrintUntil", 50));
+
+using (var rdr = cmd.ExecuteReader()) {
+    while (rdr.Read())
+    {
+        Console.WriteLine($"Number: {rdr["NUM"]}");
+    }
+}
+
+// ShopItemsInfo example. It reads data from TSQL PRINT statement.
+// It also can catch SQL exception (RAISERROR)
+Console.WriteLine("READING PRINT DATA: ");
+cmd  = new SqlCommand("ShopItemsInfo", conn);
+cmd.CommandType = CommandType.StoredProcedure;
+
+conn.InfoMessage += (sender, eventArgs) => Console.WriteLine(eventArgs.Message);
+
+try
+{
+    cmd.ExecuteNonQuery();
+}
+catch (Exception e)
+{
+    Console.WriteLine("Exception happened!");
+}
